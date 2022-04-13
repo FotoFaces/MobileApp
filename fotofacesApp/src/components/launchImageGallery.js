58cb36e237d8babd621d487365638a0ca5c1907a
JoaoBorgesUA <borgesjps@ua.pt>
2022-04-13 17:14:28 +0100

    upppppp

---
+++ fotofacesApp/src/components/launchImageGallery.js
@@ -0,0 +1,29 @@
+launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+
+  }
--- fotofacesApp/src/screens/MainScreen.js
+++ fotofacesApp/src/screens/MainScreen.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Image, View, Platform } from 'react-native'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Button from '../components/Button'
 import Paragraph from '../components/Paragraph'
 import DisplayAnImage from '../components/Image'
+import * as ImagePicker from 'expo-image-picker';
+
+
 
 
 
 export default function MainScreen({ navigation }) {
+
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <Background>
       <Header>Profile</Header>
-      <DisplayAnImage />
+      <DisplayAnImage/>
       <Paragraph>
       </Paragraph>
       <Button
@@ -22,9 +45,11 @@ export default function MainScreen({ navigation }) {
       </Button>
       <Button
         mode="outlined"
+        onPress={() => pickImage}
       >
         Gallery
       </Button>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       <Button
         mode="outlined"
         onPress={() => navigation.navigate('PhotoAccept')}
