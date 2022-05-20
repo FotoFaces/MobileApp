72361d5e77709dd47a33cfb2430e3f55a2f54e40
FlipGoncalves <filipeg@ua.pt>
2022-05-20 16:09:07 +0100

    desgin colors

--- fotofacesApp/src/core/theme.js
+++ fotofacesApp/src/core/theme.js
@@ -5,7 +5,7 @@ export const theme = {
   colors: {
     ...DefaultTheme.colors,
     text: '#000000',
-    primary: '#560CCE',
+    primary: '#5c25b4',
     title  : '#8fadaa',
     secondary: '#414757',
     error: '#f13a59',
